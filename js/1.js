5bcf22c3605cd70be37877d334bf7cf283364428
tranducchi <chitranduc9@gmail.com>
2020-11-18 10:44:01 +0700

    Them Page 2

--- js/1.js
+++ js/1.js
@@ -3,12 +3,28 @@
 $( document ).ready(function() {
     //stop next slide
     $('.carousel').carousel('pause');
-    //close menu
-    $('.close-menu').click(function(){
+
+    //open menu
+       $('.menu-button').click(function(){
+        $('span.btn1').toggleClass('rotate-right');
+        $('span.btn2').toggleClass('rotate-left');
+        $('.tab-menu').toggleClass("move-menu");
+    });
+    //close mennu click body
+    $('body').click(function(evt){    
+        if(evt.target.id == "list-menutab")
+          return;
+               if($(evt.target).closest('#list-menutab').length){
+                  return;
+        }
+        if(evt.target.id == "menu")
+          return;
+               if($(evt.target).closest('#menu').length){
+                  return;
+        }          
         $('.tab-menu').removeClass("move-menu");
+        $('span.btn1').removeClass('rotate-right');
+        $('span.btn2').removeClass('rotate-left');
     });
-     //open menu
-    $('.open-menu').click(function(){
-        $('.tab-menu').addClass("move-menu");
-    });
+    
 });
