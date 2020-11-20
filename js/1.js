5772267e4544ee2f9ce52d063b04cebf7f7d2388
tranducchi <chitranduc9@gmail.com>
2020-11-20 13:45:31 +0700

    code tong

--- js/1.js
+++ js/1.js
@@ -26,5 +26,20 @@ $( document ).ready(function() {
         $('span.btn1').removeClass('rotate-right');
         $('span.btn2').removeClass('rotate-left');
     });
+    $('#tabs-nav li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first').show();
+
+// Click function
+$('#tabs-nav li').click(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
     
 });
