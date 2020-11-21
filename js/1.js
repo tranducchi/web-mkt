f8d553584fb4c02c62d7db1338955fecb3ec5010
tranducchi <chitranduc9@gmail.com>
2020-11-21 11:19:17 +0700

    cập nhật fullpage

--- js/1.js
+++ js/1.js
@@ -55,6 +55,26 @@ $('.close').click(function(){
   $('.content-two').show();
 });
 });
+// Click to div animate
+$(document).on('click', 'a[href^="#"]', function(e) {
+// target element id
+var id = $(this).attr('href');
+
+// target element
+var $id = $(id);
+if ($id.length === 0) {
+  return;
+}
+
+// prevent standard hash navigation (avoid blinking in IE)
+e.preventDefault();
+
+// top position relative to the document
+var pos = $id.offset().top;
+
+// animated top scrolling
+$('body, html').animate({scrollTop: pos});
+});
 
 
 
